e4663cf114ea09761cf812fa769b0fc4353f4b3c
Dev.Raghad <self.speech1418@gmail.com>
2019-07-31 16:22:55 +0300

    webcam code

---
+++ Sign_language_translator_for_webcam.py
@@ -0,0 +1,50 @@
+import http.client
+import urllib.request
+import urllib.parse
+import urllib.error
+import base64
+import cv2 # this an external packege, you shuld write this command in cmd to install it for python.
+# $pip install opencv-python
+
+# for more information about this packege, go and visite "opencv.org"
+# if you work with Anaconda3 other pckeges will be installed already
+
+headers = {
+    # Request headers
+    'Prediction-Key': '<enter your predection kay>',
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-id': '<enter your Subscription id>'
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters
+    'application': '<enter your service name>'
+})
+
+# request body
+frames = 0
+seconds = 0
+cap = cv2.VideoCapture(0)
+while(cap.isOpened):
+    ret, frame = cap.read()
+    cv2.imshow('video', frame)
+    cv2.imwrite('frame.jpg', frame)
+    image = open("frame.jpg", 'rb').read()
+
+    if round(frames,1) == round(seconds,1):
+        try:
+            conn = http.client.HTTPSConnection('eastus.api.cognitive.microsoft.com') # use your endpoint here as this example
+            conn.request("POST", "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/<enter your project id>/classify/iterations/<enter your iteratin name>/image?%s"
+                % params, image, headers)
+            response = conn.getresponse()
+            data = response.read()
+            print(data)
+            conn.close()
+        except Exception as e:
+            print("[Errno {0}] {1}".format(e.errno, e.strerror))
+        seconds += 0.60
+    frames += 0.10
+
+    if cv2.waitKey(1) == ord('q'): # when you press q button it will stop all of these stuff
+        break
+cap.release()
